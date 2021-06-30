79de3fb80860bbddaae11b6d82668e7c91b99083
Graham-CO <grwi2594@colorado.edu>
2021-06-30 11:27:28 -0700

    added texture coords

--- pressure_sheet/Node.m
+++ pressure_sheet/Node.m
@@ -7,21 +7,27 @@ classdef Node
         r               % radius - for collision handling (m)
         x0              % initial position (m)
         v0 = [0,0,0]    % initial velocity (m/s)
+        u               % texture coord 1
+        v               % texture coord 2
         x               % position (m)
-        v               % velocity (m/s)
+        xd              % velocity (m/s)
         m               % mass (kg)
         fix             % fixed point switch
     end
     methods
-        function node_obj = Node(x0, m, r, fixbool)
-            if nargin < 4
+        function node_obj = Node(x0, m, r, u, v, fixbool)
+            if nargin < 6
                 node_obj.r = r;
                 node_obj.x0 = x0;
+                node_obj.u = x0(1);
+                node_obj.v = x0(2);
                 node_obj.m = m;
                 node_obj.fix = false;
             else                            % don't think is needed, but don't want the underline         
                 node_obj.r = r; 
                 node_obj.x0 = x0;
+                node_obj.u = x0(1);
+                node_obj.v = x0(2);
                 node_obj.m = m;
                 node_obj.fix = fixbool;
             end
