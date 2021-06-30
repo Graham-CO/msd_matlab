966ced1dca39f4caa7512f8738dc49b3495942a8
Graham-CO <grwi2594@colorado.edu>
2021-06-30 11:29:08 -0700

    add faceID

--- pressure_sheet/init_tris.m
+++ pressure_sheet/init_tris.m
@@ -2,6 +2,13 @@
 % Graham Williams
 % grwi2594@colorado.edu
 
-function [] = init_tris(mesh)
-    cl = mesh.ConnectivityList
+function [tris] = init_tris(mesh, nodes)
+    cl = mesh.ConnectivityList; % defines each triangle by 3 vertex IDs & faceID (for tracking textureVertexIDs)
+    
+    tris = Tri.empty(length(cl), 0); % initialize empty Tri array 
+    
+    for i = 1:length(cl)
+        tris(i) = Tri(nodes(cl(i,1)), nodes(cl(i,2)), nodes(cl(i,3)), i); 
+    end
+        
 end
