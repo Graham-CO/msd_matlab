76f68720976da92c4418db27a1a827227a9a003b
Graham-CO <grwi2594@colorado.edu>
2021-06-30 11:28:25 -0700

    add faceID

--- pressure_sheet/Tri.m
+++ pressure_sheet/Tri.m
@@ -7,23 +7,28 @@ classdef Tri
         n1     % node 1
         n2     % node 2
         n3     % node 3
+        nor    % triangle's normal
+        faceID % for tracking springs
     end
-    properties (Dependent = true) % allows calculation of these props w/o storing
-        Nor    % triangle's normal
+    
+    properties (Dependent)
         Area   % triangle's area
     end
     
     methods
-        function tri_obj = Tri(n1, n2, n3) % Class Constructor
-            % ALWAYS - only accepts 3 arguments, the three constiuent nodes
+        function tri_obj = Tri(n1, n2, n3, faceID) % Class Constructor
+            % accepts 4 arguments, the three constiuent nodes and an ID for
+            % tracking
             if nargin > 0
                 tri_obj.n1 = n1;
                 tri_obj.n2 = n2;
                 tri_obj.n3 = n3;
+                tri_obj.faceID = faceID;
+                tri_obj.nor = updateNor(tri_obj);
             end
         end
-        
-        function nor = get.Nor(tri_obj) % get function for triangle normal
+           
+        function [nor] = updateNor(tri_obj) % update function for triangle normal
             if isempty(tri_obj.n1.x) && isempty(tri_obj.n2.x) && isempty(tri_obj.n3.x)  % if the forward integration has not taken place
                 tri_obj.n1.x = tri_obj.n1.x0;   % for calculation of init nor, will clear at end 
                 tri_obj.n2.x = tri_obj.n2.x0;   % so the forward integration has an empty array to dump into
@@ -34,7 +39,7 @@ classdef Tri
                 
                 nor = cross(v,w);   % compute unit normal by taking cross product of 2/3 constiuent edges
                 
-                tri_obj.n1.x = [];
+                tri_obj.n1.x = []; % clear xcaches
                 tri_obj.n2.x = [];
                 tri_obj.n3.x = [];
             else
@@ -45,12 +50,9 @@ classdef Tri
             end
         end
         
-        function a = get.Area(tri_obj)  % get function for triangle area
-            nor = get.Nor(tri_obj); 
-            
-            a = 0.5 * norm(nor);
+        function a  = get.Area(tri_obj)  % get function for triangle area             
+            a = 0.5 * norm(tri_obj.nor);
         end
-            
     end
 end
             
