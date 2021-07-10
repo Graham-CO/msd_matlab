f050db064b54c7db2adaa879e5fcae27e8f6ffc5
Graham-CO <grwi2594@colorado.edu>
2021-07-10 12:48:27 -0600

    create a 2D mesh

---
+++ julia_port/2D_sheet_mesh.jl
@@ -0,0 +1,39 @@
+# Construct a square
+outerpnts = [Point(0.,0.), Point(0., 0.25), Point(0., 0.5), Point(0., 0.75), Point(0.,1.), Point(0.25,0.), Point(0.5, 0.), Point(0.75, 0.), Point(1., 0.),  Point(1., 1.)]
+rect = Rect(Vec(0.0,0.0), Vec(1.0,1.0))
+
+# Generate points inside of the rectangle
+innerpnts = Point[]
+
+let
+    n = 50 # Number of inner nodes
+    rmin = (3/(4*π*n))^(1/3)
+    maxiter = 1000
+    outernodes = [getindex.(outerpnts, 1) getindex.(outerpnts, 2)]
+    i = 0
+    while length(innerpnts[:,1]) ≤ n && i < maxiter
+        i += 1
+
+        innernodes = [getindex.(innerpnts, 1) getindex.(innerpnts, 2)]
+        p = Point(rand(2)...)
+
+        r1 = minimum((sum((outernodes .- p').^2, dims=2)).^(1/2))
+        r2 = isempty(innernodes) ? 1 : minimum((sum((innernodes .- p').^2, dims=2)).^(1/2))
+
+
+        if r1 > rmin && r2 > rmin && p[1] > .15 && p[1] < .85 && p[2] > .15 && p[2] < .85
+            push!(innerpnts, p)
+        end
+    end
+end
+
+outernodes = [getindex.(outerpnts, 1) getindex.(outerpnts, 2)]
+innernodes = [getindex.(innerpnts, 1) getindex.(innerpnts, 2)]
+# innernodes = collect(kmeans(innerpntsmat', 29).centers')
+
+
+
+# Triangulate the points
+triin = TriangulateIO()
+triin.pointlist = [outernodes; innernodes]'
+msh, _ = triangulate("vcDQ", triin)
