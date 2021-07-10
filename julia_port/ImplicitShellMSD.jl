64fa9ffd7dfef1dd51dab905d2e9fda970359634
Graham-CO <grwi2594@colorado.edu>
2021-07-10 12:50:27 -0600

    msh.txt now & rest lngth calc

--- julia_port/ImplicitShellMSD.jl
+++ julia_port/ImplicitShellMSD.jl
@@ -1,72 +1,41 @@
-using GeometryBasics: minimum
 using Makie
 using GeometryBasics
-using Clustering
-using Triangulate
-
-tstart = time_ns()
-function impShell() 
-    # Construct a square
-    outerpnts = [Point(0.,0.), Point(0., 0.25), Point(0., 0.5), Point(0., 0.75), Point(0.,1.), Point(0.25,0.), Point(0.5, 0.), Point(0.75, 0.), Point(1., 0.),  Point(1., 1.)]
-    rect = Rect(Vec(0.0,0.0), Vec(1.0,1.0))
-
-    # Generate points inside of the rectangle
-    innerpnts = Point[]
+using BenchmarkTools
+import DelimitedFiles: readdlm
 
 
 
+
+
+function impShell()
     
-    let
-        n = 50 # Number of inner nodes
-        rmin = (3/(4*π*n))^(1/3)
-        maxiter = 10000
-        outernodes = [getindex.(outerpnts, 1) getindex.(outerpnts, 2)]
-        i = 0
-        while length(innerpnts[:,1]) ≤ n && i < maxiter
-            i += 1
-
-            innernodes = [getindex.(innerpnts, 1) getindex.(innerpnts, 2)]
-            p = Point(rand(2)...)
-
-            r1 = minimum((sum((outernodes .- p').^2, dims=2)).^(1/2))
-            r2 = isempty(innernodes) ? 1 : minimum((sum((innernodes .- p').^2, dims=2)).^(1/2))
-
-
-            if r1 > rmin && r2 > rmin && p[1] > .15 && p[1] < .85 && p[2] > .15 && p[2] < .85
-                push!(innerpnts, p)
-            end
-        end
-    end
-
-    outernodes = [getindex.(outerpnts, 1) getindex.(outerpnts, 2)]
-    innernodes = [getindex.(innerpnts, 1) getindex.(innerpnts, 2)]
-    # innernodes = collect(kmeans(innerpntsmat', 29).centers')
-
-
-
-    # Triangulate the points
-    triin = TriangulateIO()
-    triin.pointlist = [outernodes; innernodes]'
-    msh, _ = triangulate("vcDQ", triin)
-
-
-
-
-
+    PL, CL, EL = importMesh()
+    
+    L = Array{Float64, 1}(undef,77)
+    L .= sqrt.(sum(eachrow((PL[:,EL[1,:]]-PL[:,EL[2,:]]).^2)))
+    
     # Convert to GeometryBasics.Mesh
-    pts = [Point(val[1], val[2]) for val in eachcol(msh.pointlist)]
-    fcs = [TriangleFace(val[1], val[2], val[3]) for val in eachcol(msh.trianglelist)]
+    pts = [Point3f0(val[1], val[2], val[3]) for val in eachcol(PL)]
+    fcs = [TriangleFace(val[1], val[2], val[3]) for val in eachcol(CL)] 
     msh = GeometryBasics.Mesh(pts, fcs)
+
+
 
     # Plot Mesh
     scn = Makie.mesh(msh, color = 1 : length(msh.position))
+    
     Makie.wireframe!(msh)
-
 
     display(scn)
 end
 
+function importMesh()
+    PL = readdlm("points.txt", ',', Float64)'    # Points list
+    CL = readdlm("tris.txt", ',', Int8)'         # Connectivity list
+    EL = readdlm("edges.txt", ',', Int8)'        # Edge List
+
+    return PL, CL, EL
+end
+
 
 impShell()
-tend = time_ns() - tstart
-print(tend*1e-9)
