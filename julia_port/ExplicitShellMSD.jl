9e1c7fa785dad359d34daa74857a3b7a3775b420
Graham-CO <grwi2594@colorado.edu>
2021-07-21 14:17:39 -0600

    added damping/ODE solver

--- julia_port/ExplicitShellMSD.jl
+++ julia_port/ExplicitShellMSD.jl
@@ -1,8 +1,7 @@
 using GeometryBasics
 using LinearAlgebra
-# using DifferentialEquations
+using DifferentialEquations
 import DelimitedFiles: readdlm
-using Plots
 
 # Solution Parameters
 global const p = 1              # applied pressure
@@ -28,57 +27,24 @@ function expShell()
     
     c = damp*m # damping
     
-    # 0'd arrays for holding disp/vel/accel
-    d = zeros(Float64, length(PL), 1)
-    v = zeros(Float64, length(PL), 1)
-    a = zeros(Float64, length(PL), 1)
+    # Initialize state vector
+    d₀ = reshape(PL, length(PL), 1)
+    v₀ = zeros(Float64, length(PL), 1)
+    s₀ = vcat(d₀, v₀)
+
     
-    k = Float64[] # accum vec for sparse mat NOT USING THIS RN, BC SETTING stiffness == restLength for each spring
-    
-    # Assemble force vector
+    # Initialize force vector
     f = zeros(length(PL),1)
 
     # Calculate resting length of springs
     L = Array{Float64, 1}(undef,77)  
     L .=  sqrt.(sum(eachrow((PL[:,EL[1,:]]-PL[:,EL[2,:]]).^2)))
-
-    # Add pressure forces 
-    for el = 1:size(CL,2)
-        ea = PL[:, CL[2,el]] - PL[:, CL[1,el]]
-        eb = PL[:, CL[3,el]] - PL[:, CL[1,el]]
-        fe = (p/6)*cross(ea,eb)
-        ne = [3*CL[1,el].-[2; 1; 0] 3*CL[2,el].-[2;1;0] 3*CL[3,el].-[2; 1; 0]]
-        f[ne] = repeat(fe,3,1)    
-    end
     
-    # Add spring forces (should also calc damp forces here) 
-    for ele = 1:size(EL,2)
+    params = (PL, CL, EL, L, m, f, c)
+    display(params)
 
-        V = [ PL[:, EL[1,ele]]+d[3*EL[1,ele].-[2; 1; 0;]]   PL[:, EL[2,ele]]+d[3*EL[2,ele].-[2; 1; 0;]] ] # Coords of nodes belonging to this spring
-        dL = sqrt(sum((V[:,2] - V[:,1]).^2)) # Length of deformed spring (after t = 0)
-          
-        ne = [3*EL[1,ele].-[2; 1; 0] 3*EL[2,ele].-[2; 1; 0]] # Indexing for each node within force vector 
-
-        f[ne[:,1]] .+= -L[ele]*(dL - L[ele])*(V[:,2] - V[:,1])/dL # For node1 of edge, f = k*(norm(pt2-pt1) - restLength)*(pt2-pt1)/norm(pt2-pt1)
-        f[ne[:,2]] .+= -L[ele]*(dL - L[ele])*(V[:,1] - V[:,2])/dL # Same for node2 of edge, except direction flipped, so that f = k*(norm(pt2-pt1) - restLength)*(pt1-pt2)/norm(pt2-pt1)
-
-    end
-    
-
-    # params = (m, f, c, k)
-
-    # function accel(a, v, d, params) 
-    #     m, f, c, k = params
-
-    #     @. a = 1/m  * (f - (c*v + k*d))
-        
-    #     return
-    # end
-    
-
-
-    # prob = SecondOrderODEProblem(accel, v, d, params, (0.0,10.0))
-    # sol = solve(prob)
+    prob = ODEProblem(accel, s₀, (0.0,10.0), params)
+    sol = solve(prob)
 
 
 end
@@ -92,5 +58,45 @@ function importMesh()
     return PL, CL, EL
 end
 
+function accel(a, s, params, t) 
+    PL, CL, EL, L, m, f, c = params
 
+    # Add pressure forces 
+    for el = 1:size(CL,2)
+        ea = (PL[:, CL[2,el]]+s[3*CL[2,el].-[2; 1; 0;]]) - (PL[:, CL[1,el]]+s[3*CL[1,el].-[2; 1; 0;]])
+        eb = (PL[:, CL[3,el]]+s[3*CL[3,el].-[2; 1; 0;]]) - (PL[:, CL[1,el]]+s[3*CL[1,el].-[2; 1; 0;]])
+        fe = (p/6)*cross(ea,eb)
+        ne = [3*CL[1,el].-[2; 1; 0] 3*CL[2,el].-[2;1;0] 3*CL[3,el].-[2; 1; 0]]
+        f[ne] = repeat(fe,3,1)    
+    end
+
+    # Add spring & damping forces  
+    for ele = 1:size(EL,2)
+
+        P = [ PL[:, EL[1,ele]]+s[3*EL[1,ele].-[2; 1; 0;]]   PL[:, EL[2,ele]]+s[3*EL[2,ele].-[2; 1; 0;]] ] # Coords of nodes belonging to this spring
+        V = [ s[(3*EL[1,ele].-[2; 1; 0;]).+96]     s[(3*EL[2,ele].-[2; 1; 0;]).+96] ] # Velocities of nodes belonging to this spring
+        
+        dL = sqrt(sum((P[:,2] - P[:,1]).^2)) # Length of deformed spring (after t = 0)
+        
+        disp = dL - L[ele] # Displacement from rest
+        relV = (V[:,1] - V[:,2]) # Relative velocity
+        n_hat = (P[:,2] - P[:,1])/dL # Directional unit vector
+        
+        spr = L[ele]*disp # Spring force
+        damp = c[ele]*relV*n_hat # Damping force
+          
+        ne = [3*EL[1,ele].-[2; 1; 0] 3*EL[2,ele].-[2; 1; 0]] # Indexing for each node within force vector 
+
+        f[ne[:,1]] .+= -(spr + damp)*n_hat # For node1 of edge, f = k*(norm(pt2-pt1) - restLength)*(pt2-pt1)/norm(pt2-pt1)
+        f[ne[:,2]] .+= -(spr + damp)*-n_hat # Same for node2 of edge, except direction flipped, so that f = k*(norm(pt2-pt1) - restLength)*(pt1-pt2)/norm(pt2-pt1)
+
+    end
+
+    a[1:96] = s[1:96]
+
+    a[97:192] .= f ./ m
+
+    
+    return
+end
 expShell()
