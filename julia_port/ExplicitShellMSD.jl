af2106eff4c02a99ae4d00f977efe6248b3ddca0
Graham-CO <grwi2594@colorado.edu>
2021-07-26 10:39:48 -0600

    changed damping coefficient to 1/damp*m

--- julia_port/ExplicitShellMSD.jl
+++ julia_port/ExplicitShellMSD.jl
@@ -1,10 +1,12 @@
 using GeometryBasics
 using LinearAlgebra
 using DifferentialEquations
+using PrettyTables
+import GLMakie as mk
 import DelimitedFiles: readdlm
 
 # Solution Parameters
-global const p = 1              # applied pressure
+global const p = 0.01              # applied pressure
 global const fixed = 1:48       # fixed nodes (edges)
 global const th = 0.1           # material thickness
 global const rho = 10           # material density
@@ -21,7 +23,7 @@ function expShell()
         eb = PL[:, CL[3,el]] - PL[:, CL[1,el]]
         A = norm(cross(ea,eb)/2)
         ne = [3*CL[1,el].-[2; 1; 0] 3*CL[2,el].-[2; 1; 0] 3*CL[3,el].-[2; 1; 0]]
-        m[ne] .+= th*rho*A/3    # mass is calc'd as th*density*area/3 - three is since 3 points on tri --- iterated for each triangle the mass belongs to
+        m[ne] .+= (th*rho*A/3)*10000  # mass is calc'd as th*density*area/3 - three is since 3 points on tri --- iterated for each triangle the mass belongs to
     end
     m[fixed] .= 1e4
     
@@ -31,7 +33,6 @@ function expShell()
     d₀ = reshape(PL, length(PL), 1)
     v₀ = zeros(Float64, length(PL), 1)
     s₀ = vcat(d₀, v₀)
-
     
     # Initialize force vector
     f = zeros(length(PL),1)
@@ -41,10 +42,23 @@ function expShell()
     L .=  sqrt.(sum(eachrow((PL[:,EL[1,:]]-PL[:,EL[2,:]]).^2)))
     
     params = (PL, CL, EL, L, m, f, c)
-    display(params)
+
+    # du = similar(s₀)
+    # accel(du,s₀, params, 0.0)
 
     prob = ODEProblem(accel, s₀, (0.0,10.0), params)
     sol = solve(prob)
+
+    
+    times = sol.t
+    states = sol.u
+
+    plotMesh(states, EL, times)
+    # @pt times
+    # @pt states
+
+
+    
 
 
 end
@@ -63,40 +77,75 @@ function accel(a, s, params, t)
 
     # Add pressure forces 
     for el = 1:size(CL,2)
-        ea = (PL[:, CL[2,el]]+s[3*CL[2,el].-[2; 1; 0;]]) - (PL[:, CL[1,el]]+s[3*CL[1,el].-[2; 1; 0;]])
-        eb = (PL[:, CL[3,el]]+s[3*CL[3,el].-[2; 1; 0;]]) - (PL[:, CL[1,el]]+s[3*CL[1,el].-[2; 1; 0;]])
+        ea = (s[3*CL[2,el].-[2; 1; 0;]]) - (s[3*CL[1,el].-[2; 1; 0;]])
+        eb = (s[3*CL[3,el].-[2; 1; 0;]]) - (s[3*CL[1,el].-[2; 1; 0;]])
         fe = (p/6)*cross(ea,eb)
-        ne = [3*CL[1,el].-[2; 1; 0] 3*CL[2,el].-[2;1;0] 3*CL[3,el].-[2; 1; 0]]
-        f[ne] = repeat(fe,3,1)    
+        ne = [3*CL[1,el].-[2; 1; 0]; 3*CL[2,el].-[2;1;0]; 3*CL[3,el].-[2; 1; 0]]
+        
+        f[ne] += repeat(fe,3,1)
     end
-
+    bkpt =1
     # Add spring & damping forces  
     for ele = 1:size(EL,2)
 
-        P = [ PL[:, EL[1,ele]]+s[3*EL[1,ele].-[2; 1; 0;]]   PL[:, EL[2,ele]]+s[3*EL[2,ele].-[2; 1; 0;]] ] # Coords of nodes belonging to this spring
+        P = [ (s[3*EL[1,ele].-[2; 1; 0;]])   (s[3*EL[2,ele].-[2; 1; 0;]]) ] # Coords of nodes belonging to this spring
         V = [ s[(3*EL[1,ele].-[2; 1; 0;]).+96]     s[(3*EL[2,ele].-[2; 1; 0;]).+96] ] # Velocities of nodes belonging to this spring
-        
+
         dL = sqrt(sum((P[:,2] - P[:,1]).^2)) # Length of deformed spring (after t = 0)
         
         disp = dL - L[ele] # Displacement from rest
-        relV = (V[:,1] - V[:,2]) # Relative velocity
+        relV = (V[:,2] - V[:,1]) # Relative velocity
+
         n_hat = (P[:,2] - P[:,1])/dL # Directional unit vector
         
         spr = L[ele]*disp # Spring force
-        damp = c[ele]*relV*n_hat # Damping force
+        damp = dot((1/c[ele]).*relV, n_hat) # Damping force
           
         ne = [3*EL[1,ele].-[2; 1; 0] 3*EL[2,ele].-[2; 1; 0]] # Indexing for each node within force vector 
 
-        f[ne[:,1]] .+= -(spr + damp)*n_hat # For node1 of edge, f = k*(norm(pt2-pt1) - restLength)*(pt2-pt1)/norm(pt2-pt1)
-        f[ne[:,2]] .+= -(spr + damp)*-n_hat # Same for node2 of edge, except direction flipped, so that f = k*(norm(pt2-pt1) - restLength)*(pt1-pt2)/norm(pt2-pt1)
+        neg = -n_hat
+        
+        f[ne[:,1]] += -(spr + damp).*n_hat 
+        f[ne[:,2]] += -(spr + damp).*(-n_hat) 
+        
+    end
+    bkpt = 1
+    a[1:96] = s[97:192]
+    a[97:192] = f ./ m
 
+    return a
+end
+
+function plotMesh(s, EL, t)
+    # fcs = [TriangleFace(val[1], val[2], val[3]) for val in eachcol(CL)]
+    # pts = Array{Vector{Point{3, Float32}}, 1}(undef, size(states,1))
+    # for i = 1:size(states,1)
+    #     pts[i] = [Point3f0(val[1], val[2], val[3]) for val in eachrow(reshape(states[i][1:96],3,32)')]
+    # end
+
+    # msh = GeometryBasics.Mesh(pts, fcs)
+    # msh = mk.wireframe!(msh)
+    # scn = mk.mesh(msh, color = 1 : length(msh.position), camera = cam3d!)
+
+    scene = mk.Scene()
+
+    pts = Array{Vector{Float64}, 1}(undef,size(s,1))
+    # lines = Array{Float64,2}(undef, )
+
+
+    for i ∈ 1:size(s, 1)
+        pts[i] = s[i][1:96]
     end
 
-    a[1:96] = s[1:96]
+    # for i ∈ 1:size(s,1)
+    #     for j ∈ 1:size(EL,2)
+    #         lines[2*i-1,:] =s[i, 3*EL[1,j].-[2;1;0;]]
+    #         lines[2*i,:] = s[i, 3*EL[2,j].-[2;1;0]]
+    #     end
+    # end
+    # @pt lines
+    @pt pts
 
-    a[97:192] .= f ./ m
-
-    
-    return
+    # display(scn)
 end
 expShell()
